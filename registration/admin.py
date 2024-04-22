67c2783a40b0e94072bd1bc78a4db3aecdb2edc8
DanielMoko <mokombugua.info@gmail.com>
2024-04-22 09:47:03 +0300

    homemod

--- registration/admin.py
+++ registration/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(Student)
+from .models import Data
+# Register your models here
 admin.site.register(Data)
